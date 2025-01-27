1ff58b56d49f77862c01a5cab3cd61a654799347
Anish Sharma <learninghowtoprogramme@gmail.com>
2025-01-27 23:45:30 +0530

    Basic practice

---
+++ Basic_1/Basic_Practice.py
@@ -0,0 +1,7 @@
+# just putted my first python command
+print ("First printing anytjhing on python")
+# Function creation
+def chai(a):
+    print(a)
+
+chai('Lemon Tea')
---
+++ Basic_1/Function Calling.py
@@ -0,0 +1,3 @@
+# Calling Function from another file
+from Basic_Practice import chai
+chai("Super Lemon Tea")
