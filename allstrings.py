47c35bf9ad4e512dd0993a0edd0885b767d02fb7
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-02-27 20:00:10 +0530

    strings

---
+++ allstrings.py
@@ -0,0 +1,15 @@
+# In this class we will learn all about string manipulation
+chai = "Masala Chai"
+print (chai[0]) # 1st character is printed
+print (chai[0:6]) # total 5 characters 'Masala' is printed
+
+# Now number slicing
+number_list = "0123456789"
+print (number_list)
+print (number_list [:]) # number_list and number_list [:] both are same
+print (number_list [0:]) # number_list, number_list [:], number_list [0:] all are same
+print (number_list [0:5]) # non inclusive shows upto only 4 index number
+print (number_list [-4]) # non inclusive and from right side
+print (number_list [1:-3])
+print (number_list [5])
+print (number_list [5:-2]) # done 8.36 minutes
